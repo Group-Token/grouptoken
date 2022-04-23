a63896c08d702389d2313b68623587362dbe5545
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-04-22 20:44:43 -0700

    Added Loading Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,52 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage/';
 import { Routes, Route } from "react-router-dom";
 import { useSelector, useDispatch } from 'react-redux';
 import { setKey } from './features/publicKey/publicKeySlice';
+import LoadingPage from './components/LoadingPage/LoadingPage';
 
 function App() {
   const publicKey = useSelector(state => state.publicKey.value);
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    window.solana.connect({ onlyIfTrusted: true }).then(
-      ({ publicKey }) => {
-        dispatch(setKey(publicKey.toBase58()));
-      }
-    ).catch(
-      (err) => {
-        console.log(err);
-      }
-    );
+    if ("solana" in window) {
+      window.solana.connect({ onlyIfTrusted: true }).then(
+        ({ publicKey }) => {
+          dispatch(setKey(publicKey.toBase58()));
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+        }
+      ).catch(
+        (err) => {
+          console.log(err);
+          setTimeout(() => {
+            setLoading(false);
+          }, 1000);
+        }
+      );
+    } else {
+      window.open("https://phantom.app/", "_blank");
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
   });
 
   return (
     <div className="App">
-       <Routes>
-        <Route path="/" element={publicKey == null ? <LandingPage/> : <><h1>Key: {publicKey.payload}</h1></>} />
-       </Routes>
+      {
+        loading ? (
+          <LoadingPage />
+        ) : (
+          <Routes>
+            <Route path="/" element={publicKey == null ? <LandingPage/> : <><h1>Key: {publicKey.payload}</h1></>} />
+          </Routes>
+        )
+      }
     </div>
   );
 }
