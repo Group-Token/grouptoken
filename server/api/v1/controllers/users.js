e04dcc0153385cd2f28bcfc35c3cd69d6e8e578f
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-05-09 20:56:07 -0700

    Added Tokens Page

--- server/api/v1/controllers/users.js
+++ server/api/v1/controllers/users.js
@@ -2,69 +2,68 @@ const { faker } = require('@faker-js/faker');
 const axios = require('axios');
 
 module.exports = function (app, prisma) {
-  app.get('/api/v1/users/:publicKey', (req, res) => {
-    prisma.User.findUnique({
+  const config = {
+    headers: {
+      "accept": "application/json",
+      "X-API-KEY": process.env.MORALIS_API_KEY,
+    },
+  }
+
+  const createUser = async (publicKey) => {
+    const response = axios.get(`https://solana-gateway.moralis.io/account/mainnet/${publicKey}/portfolio`, config);
+    const user = await prisma.User.create({
+      data: {
+        publicKey: publicKey,
+        username: `${faker.word.adjective()}${faker.animal.type()}`,
+        imageURL: `https://avatars.dicebear.com/api/big-ears/${publicKey}.png`,
+      }
+    });
+    return user
+  }
+
+  const getTokens = async (publicKey) => {
+    const response = await axios.get(`https://solana-gateway.moralis.io/account/mainnet/${publicKey}/portfolio`, config);
+    const tokens = [];
+
+    for (nft of response.data.nfts) {
+      const token = await getTokenData(nft);
+      tokens.push(token);
+    }
+    
+    return tokens;
+  }
+
+  const getTokenData = async (token) => {
+    const response = await axios.get(`https://solana-gateway.moralis.io/nft/mainnet/${token.mint}/metadata`, config);
+    const data = response.data;
+    const standard = data.standard;
+    const metadataURI = data[standard]['metadataUri'];
+    const nft = await getTokenMetadata(metadataURI);
+    nft.address = data.mint;
+    return nft;
+  }
+
+  const getTokenMetadata = async (metadataURI) => {
+    const tokenMetadata = await axios.get(metadataURI);
+    const nft = {
+      name: tokenMetadata.data.name,
+      imageURL: tokenMetadata.data.image,
+      description: tokenMetadata.data.description,
+    }
+    return nft;
+  }
+
+  app.get('/api/v1/users/:publicKey', async (req, res) => {
+    const user = await prisma.User.findUnique({
       where: {
         publicKey: req.params.publicKey,
       }
-    }).then(
-      (user) => {
-        res.json(user);
-      }
-    ).catch(
-      (err) => {
-        console.log(err);
-        res.status(404).json({ message: "User not found" });
-      }
-    );
-  });
-
-  app.post('/api/v1/users', (req, res) => {
-    const config = {
-      headers: {
-        "accept": "application/json",
-        "X-API-KEY": process.env.MORALIS_API_KEY,
-      },
+    });
+    if (!user) {
+      user = await createUser(req.params.publicKey);
     }
-    axios.get(`https://solana-gateway.moralis.io/account/mainnet/${req.body.publicKey}/portfolio`, config).then(
-      (response) => {
-        prisma.User.create({
-          data: {
-            publicKey: req.body.publicKey,
-            username: `${faker.word.adjective()}${faker.animal.type()}`,
-            imageURL: `https://avatars.dicebear.com/api/big-ears/${req.body.publicKey}.png`,
-          }
-        }).then(
-          (user) => {
-            const portfolio = response.data;
-            
-            portfolio.nfts.forEach((token) => {
-              prisma.Token.upsert({
-                where: {
-                  address: token.associatedTokenAddress,
-                },
-                update: {
-                  ownerAddress: user.publicKey,
-                },
-                create: {
-                  address: token.associatedTokenAddress,
-                  ownerAddress: user.publicKey,
-                }
-              });
-            });
-            
-            res.json(user);
-          }
-        ).catch(
-          (err) => {
-            console.log(err);
-          }
-        )
-      }
-    ).catch(
-      (err) => {
-        console.log(err);
-      }
-    );
+    user.tokens = await getTokens(req.params.publicKey);
+
+    res.json(user);
   });
 }
